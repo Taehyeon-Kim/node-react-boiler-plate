1dc92966c5719b20736e4aa70f7fed586bb3b434
Taehyeon Kim <taehyk528@gmail.com>
2020-07-17 23:37:19 +0900

    [UPDATE] Mongoose 설치 및 MongoDB 연결

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://taehyeon:taehyeon1234@node-boiler-plate.82pg5.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
